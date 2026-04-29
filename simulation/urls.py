0c5635382ccd53fc76e3959007abc9310b625a16
Mburu Sam <mburusam885@gmail.com>
2026-04-29 21:23:48 +0300

    files update

--- simulation/urls.py
+++ simulation/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import dashboard, export_excel
+from .views import dashboard, export_excel, compare
 
 urlpatterns = [
     path('', dashboard, name='dashboard'),
     path('export/', export_excel, name='export_excel'),
+    path('compare/', compare, name='compare'),
 ]
--- simulation/views.py
+++ simulation/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from openpyxl import Workbook
-from .models import run_simulation
+from .models import run_simulation, SimulationRun
 
 def export_excel(request):
     data = run_simulation()
@@ -28,7 +28,46 @@ def export_excel(request):
 
     wb.save(response)
     return response
-from .models import SimulationRun
+
+
+def compare(request):
+    """Compare two simulations side by side"""
+    sim1_id = request.GET.get('sim1')
+    sim2_id = request.GET.get('sim2')
+    
+    simulations = SimulationRun.objects.order_by('-created_at')[:10]
+    
+    sim1 = None
+    sim2 = None
+    
+    if sim1_id:
+        sim1 = get_object_or_404(SimulationRun, id=sim1_id)
+    if sim2_id:
+        sim2 = get_object_or_404(SimulationRun, id=sim2_id)
+    
+    # Calculate stats for comparison
+    def calc_stats(sim):
+        if not sim:
+            return None
+        return {
+            'avg_price': sum(sim.prices) / len(sim.prices),
+            'total_demand': sum(sim.demand),
+            'total_sales': sum(sim.sales),
+            'max_price': max(sim.prices),
+            'min_price': min(sim.prices),
+        }
+    
+    stats1 = calc_stats(sim1)
+    stats2 = calc_stats(sim2)
+    
+    return render(request, 'simulation/compare.html', {
+        'simulations': simulations,
+        'sim1': sim1,
+        'sim2': sim2,
+        'stats1': stats1,
+        'stats2': stats2,
+    })
+
 
 def dashboard(request):
     if request.method == "POST":
